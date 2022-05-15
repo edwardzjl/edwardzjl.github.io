7eb6df9fdec670f7c75a58e56490cd268e715eee
Junlin Zhou <jlzhou@zjuici.com>
2022-05-15 15:41:54 +0800

    update: config deploy branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'edwardzjl', // Usually your GitHub org/user name.
   projectName: 'edwardzjl.github.io', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
 
   trailingSlash: false,
 
