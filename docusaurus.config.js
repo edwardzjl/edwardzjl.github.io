67ec6246c0c5ac33776a44a3147871071998411a
Junlin Zhou <jlzhou@zjuici.com>
2022-05-15 16:40:16 +0800

    update: links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,14 +40,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/edwardzjl/edwardzjl.github.io/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/edwardzjl/edwardzjl.github.io/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -118,7 +118,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/edwardzjl/edwardzjl.github.io',
               },
             ],
           },
